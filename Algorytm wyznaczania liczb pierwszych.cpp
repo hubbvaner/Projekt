279b512ed1da1663b0aa3592f6e85de51a3dc761
hubbvaner <74739479+hubbvaner@users.noreply.github.com>
2022-04-03 21:28:34 +0200

    Add files via upload

---
+++ Algorytm wyznaczania liczb pierwszych.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	int	j;
+	int	liczenie = 1;
+	int	b = 0;
+	int ilosc = 0;
+
+	cout << "podaj ilosc liczb\n" << endl;
+
+	cin >> ilosc;
+
+	for (i = 3; i > 0; ++i)
+	{
+		for (j = 2; j <= i / 2; ++j)
+		{
+			if (i % j == 0) {
+				b = 1;
+				break;
+			}
+		}
+		if (b == 0)
+		{
+			cout << "\n" << i;
+			liczenie++;
+		}
+		b = 0;
+		if (liczenie == ilosc + 1)
+			break;
+	}
+
+	return 0;
+}
