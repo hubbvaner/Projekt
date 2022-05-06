f9a3f1bee56b23d76a795f9f10f5364a52c800d8
hubbvaner <74739479+hubbvaner@users.noreply.github.com>
2022-05-06 13:47:24 +0200

    Add files via upload

---
+++ ciag feaifiafias/ciag feaifiafias/ciag feaifiafias.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+using namespace std;
+
+void fib(int n)
+{
+    int f0 = 0;
+    int f1 = 1;
+    int f;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 1) {
+            f = f0 + f1;
+            f0 = f1;
+            f1 = f;
+        }
+        else
+        {
+            f = i;
+        }
+        cout << f << " ";
+        
+    }
+}
+int main()
+
+{
+    int n;
+    cout << "Podaj ilosc: ";
+    cin >> n;
+
+    fib(n);
+
+        return 0;
+}
